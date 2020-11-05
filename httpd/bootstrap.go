6bdd1e96236316398c351028d9d7b1ced8cb94a9
Ryan Back <ryanpback@gmail.com>
2020-11-04 18:55:52 -0800

    Clean up config

--- httpd/bootstrap.go
+++ httpd/bootstrap.go
@@ -11,15 +11,12 @@ var appConfig bs.Config
 
 func bootstrap() {
 	c, err := bs.InitConfig(helpers.GetEnv(isTesting))
-	helpers.AppConfig = c
-
 	if err != nil {
-		helpers.LogFatal(err)
-		helpers.LogFatal(appConfig)
-		panic("Someting went wrong, check your environment variables")
+		panic(err)
 	}
 
 	appConfig = c
+	helpers.AppConfig = c
 	models.AppConfig = c
 	handlers.AppConfig = c
 }
